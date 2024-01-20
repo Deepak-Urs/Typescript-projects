f3a8070c04f44d6408544145a09a0077fd28b8a8
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-01-20 13:55:26 -0800

    arrays and tuples

--- core/src/index.ts
+++ core/src/index.ts
@@ -5,3 +5,14 @@ let x: any = 'hello'
 
 // isPublished = 1 --> error
 // x = true --> no error
+
+// arrays
+let ids: number[] = [1,2,3,4,5]
+let arr: any[] = [1, true, 'hi']
+
+//Tuples
+let person: [number, string, boolean] = [1, 'abcd', true]
+// Tuple arrays
+let employee: [number, string][] = [
+    [1, 'a'], [2, 'b'], [3, 'c'], [4, 'd']
+]
