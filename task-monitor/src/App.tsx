f9dade10f6ccc494bae08a52fd4b4784e6e5ac26
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-01-23 14:15:15 -0800

    task-master base project setup

---
+++ task-monitor/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+// React.Node supports all
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <span className="heading">Task Monitor</span>
+    </div>
+  );
+}
+
+export default App;
