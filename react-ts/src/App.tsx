0a486cd2118c2f969d3809457451a343ecf701b2
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-01-23 13:49:46 -0800

    basics + extending types

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -3,10 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 
+let name: string;
+let age: number | string; //-> union
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string]; //-> Tuple
+
+//object
+type Person = {
+  age?: number
+  name: string
+}
+let person: Person = {
+  name: "Deepak",
+}
+
+// extending TYPEs
+type P2 = Person & {
+  id: number,
+  address: string
+}
+let totalPerson: P2 = {
+  id: 1,
+  address: 'abc',
+  age: 27,
+  name: 'Deepak'
+}
+console.log(totalPerson);
+
+
+//function printName(name:String) {
+//  console.log(name);
+//}
+//printName('Deepak')
+
+let printName: (name: string) => void;
+//printName('Deepak')
+
+
 function App() {
   return (
     <div className='App'>
       <Header title='Hello World' />
+      
     </div>  
   );
 }
