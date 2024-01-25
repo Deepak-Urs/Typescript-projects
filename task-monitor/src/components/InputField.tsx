04877a628673ae9dd37583c1670d083b345206df
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-01-25 15:29:51 -0800

    Create Todo Logic

--- task-monitor/src/components/InputField.tsx
+++ task-monitor/src/components/InputField.tsx
@@ -1,13 +1,14 @@
 import "./styles.css"
 interface Props{
     todo: String,
-    setTodo: React.Dispatch<React.SetStateAction<string>>
+    setTodo: React.Dispatch<React.SetStateAction<string>>,
+    handleAdd: (e: React.FormEvent) => void;
 }
 
 //const InputField: React.FC<Props> = ({ todo, setTodo}) => {
-const InputField = ({ todo, setTodo}: Props) => {
+const InputField = ({ todo, setTodo, handleAdd}: Props) => {
   return (
-    <form className='input'>
+    <form className='input' onSubmit={handleAdd}>
     <input type='input' 
         onChange={(e) => setTodo(e.target.value)} placeholder='Enter a task' className='input__box'/>
     <button className='input__submit' type='submit'>Go</button>
