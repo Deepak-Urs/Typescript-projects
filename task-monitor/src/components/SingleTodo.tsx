4c9dbf71720deb43f7523b95dd1b8c083be4ba35
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-01-26 14:36:33 -0800

    handleDone functionality done

--- task-monitor/src/components/SingleTodo.tsx
+++ task-monitor/src/components/SingleTodo.tsx
@@ -6,19 +6,30 @@ import './styles.css'
 type Props = {
     todo: Todo,
     todos: Todo[],
-    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
+    setTodos: React.Dispatch<React.SetStateAction<any[]>>
 }
 const SingleTodo: React.FC<Props> = ({todo, todos, setTodos}) => {
-  return (
+  const handleDone = (id: number) => {
+    setTodos(
+        todos.map(todo=> 
+            todo.id === id ? {...todo, isDone: !todo.isDone} :todo
+        ));
+  };
+  
+    return (
     <form className="todos__single">
-        <span className="todos__single--text">
-            {todo.todo}
-        </span>
-
+        {
+            !todo.isDone ? (
+            <s className="todos__single--text">{todo.todo}</s>
+            ): (
+            <span className="todos__single--text">{todo.todo}</span>
+            )
+        }
+        
         <div>
             <span className="icon"><AiFillEdit /></span>
             <span className="icon"><AiFillDelete /></span>
-            <span className="icon"><MdDone /></span>
+            <span className="icon" onClick={() => handleDone(todo.id)}><MdDone /></span>
         </div>
       
     </form>
